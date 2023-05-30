2eb5abf0257dc11ed446e1114e2e76a8ddac4e7a
Kevin Christian <kdotchrist30@gmail.com>
2023-05-30 16:49:24 +0700

    Added Login UI

---
+++ Queries.sql
@@ -0,0 +1,15 @@
+USE beetest
+GO
+
+DROP TABLE Participant_Schedules
+DROP TABLE Participants
+DROP TABLE Questions
+DROP TABLE QuestionTypes
+DROP TABLE Schedules
+DROP TABLE TemporaryAnswers
+DROP TABLE Tests
+
+SELECT * FROM QuestionTypes
+GO
+
+SELECT * FROM Participants
